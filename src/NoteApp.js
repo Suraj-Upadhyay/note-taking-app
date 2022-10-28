abf71e556b6cb6485ebd5e034bf93ee5c53a3174
Suraj Upadhyay <usuraj35@gmail.com>
2022-10-28 15:44:12 +0530

    Add Note App Top Level Functionality

--- src/NoteApp.js
+++ src/NoteApp.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Search from './components/Search';
 import Profile from './components/Profile'
 import Notes from './components/Notes';
 import AddNote from './components/AddNote';
 import './styles/note_app.css';
+import userNotes from './UserNotes/UserNotes';
 
 export default function NoteApp() {
+    const [notes, setNotes] = useState(userNotes);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const onAddNote = (newNote) => {
+        setNotes((prevNotes) =>
+            [...prevNotes, newNote]
+        );
+    };
+
+    const onEditNote = (index, editedNote) => {
+        const sameNodes = notes.filter((note, noteIndex) => noteIndex !== index);
+        setNotes((prevNotes) => [
+            ...sameNodes,
+            editedNote
+        ]);
+    };
+
+    const onSearch = (searchFor) => {
+        setSearchTerm(searchFor);
+    };
+
     return (
         <div>
             <div className='nav-bar'>
                 <div></div>
                 <div className='search-bar'>
-                    <Search />
+                    <Search searchTerm={searchTerm} onSearch={onSearch}/>
                 </div>
                 <div className='profile-icon'>
-                    <AddNote />
+                    <AddNote onAddNote={onAddNote}/>
                     <Profile />
                 </div>
             </div>
             <div className='body'>
                 <div></div>
-                <Notes />
+                <Notes notes={notes} searchTerm={searchTerm} onEditNote={onEditNote}/>
             </div>
         </div>
     );
