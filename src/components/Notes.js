124a7ab055b148a779270bbd2ce695fe9ac6c335
Suraj Upadhyay <usuraj35@gmail.com>
2022-10-28 16:19:59 +0530

    Add Notes Top Level Functionality

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import Note from './Note';
+import PropTypes from 'prop-types';
 import '../styles/notes.css';
 
-export default function Notes() {
+export default function Notes(props) {
     return (
         <div className='notes-container'>
-            <Note />
-            <Note />
-            <Note />
-            <Note />
+            {props.notes.filter(
+                    (note, index) =>
+                        note.text.includes(props.searchTerm)
+                )
+                .map(
+                    (note, index) => 
+                        <Note note={note} key={index} onEdit={props.onEditNote}/>
+                )
+            }
         </div>
     );
 }
+
+Notes.propTypes = {
+    notes: PropTypes.array.isRequired,
+    searchTerm: PropTypes.string.isRequired,
+    onEditNote: PropTypes.func.isRequired
+};
